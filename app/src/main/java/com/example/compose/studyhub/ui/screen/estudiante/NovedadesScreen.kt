5b65786a0d9caea343667518abc9cae9ba79d352
ElDeLaBarba <thatonewithabeard@gmail.com>
2024-06-25 10:55:38 -0300

    Added a dash in NovedadesScreen

--- app/src/main/java/com/example/compose/studyhub/ui/screen/estudiante/NovedadesScreen.kt
+++ app/src/main/java/com/example/compose/studyhub/ui/screen/estudiante/NovedadesScreen.kt
@@ -9,10 +9,12 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
@@ -32,6 +34,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.compose.studyhub.data.UserRepository
 import com.example.compose.studyhub.ui.component.Logo
 import com.example.compose.studyhub.ui.navigation.LogoutBox
@@ -44,14 +47,20 @@ import kotlinx.coroutines.launch
 @Composable
 fun NovedadesScreen(navController: NavHostController): DrawerState {
    val showLogoutDialog = remember { mutableStateOf(false) }
-   val nombre = UserRepository.getNombre()?: ""
+   val nombre = UserRepository.getNombre() ?: ""
 
    val greetings = "Bienvenido, $nombre"
 
-   var displayText by remember {mutableStateOf("")}
+   var displayText by remember { mutableStateOf("") }
    val scope = rememberCoroutineScope()
+   var done by remember { mutableStateOf(false) }
+   var writingBar by remember { mutableStateOf("|") }
 
-   Column(modifier = Modifier.fillMaxSize().padding(bottom=200.dp, start=0.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+   Column(
+      modifier = Modifier.fillMaxSize().padding(bottom = 200.dp, start = 0.dp),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+   ) {
       //Image(painter = painterResource(id = R.drawable.logotext), modifier = Modifier.size(220.dp), contentDescription = "Logo")
       //Text("Novedades", style = MaterialTheme.typography.titleMedium, color = md_theme_dark_text)
 
@@ -69,7 +78,7 @@ fun NovedadesScreen(navController: NavHostController): DrawerState {
 
       //Efecto para el texto
       LaunchedEffect(greetings) {
-         if(nombre!=""){
+         if (nombre != "") {
             scope.launch {
                val stringBuilder = StringBuilder()
                for (letter in greetings) {
@@ -77,42 +86,68 @@ fun NovedadesScreen(navController: NavHostController): DrawerState {
                   delay(70)
                   displayText = stringBuilder.toString()
                }
+               done = true
             }
          }
 
+
+      }
+
+      LaunchedEffect(done) {
+         val fullText = writingBar
+         while (true) {
+            delay(150)
+            writingBar = " "
+            delay(150)
+            writingBar = fullText
+         }
       }
 
 
       Column(modifier = Modifier.wrapContentHeight(align = Alignment.CenterVertically)) {
-         Logo(modifier = Modifier
-            .align(Alignment.CenterHorizontally)
-            .size(240.dp)
-            .scale(animatedScale)
-            .padding(top = 76.dp))
+         Logo(
+            modifier = Modifier
+               .align(Alignment.CenterHorizontally)
+               .size(240.dp)
+               .scale(animatedScale)
+               .padding(top = 76.dp)
+         )
 
 
 
 
+         Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+               .fillMaxWidth()
+               .padding(top = 30.dp)) {
+            Text(
+               text = displayText,
+               style = MaterialTheme.typography.headlineLarge,
+               textAlign = TextAlign.Center,
+            )
+            Text(
+               text = writingBar,
+               style = MaterialTheme.typography.headlineLarge,
+               textAlign = TextAlign.Center,
+               modifier = Modifier.width(13.dp)
+            )
+         }
 
-         Text(text = displayText, style = MaterialTheme.typography.headlineLarge, textAlign = TextAlign.Center, modifier = Modifier
-            .padding(top = 30.dp)
-            .fillMaxWidth())
+
+
+         BackHandler {
+            showLogoutDialog.value = true
+         }
+
+         if (showLogoutDialog.value) {
+            LogoutBox(navController = navController, onDismiss = { showLogoutDialog.value = false })
+         }
+
+
 
       }
    }
-
-
-   BackHandler{
-      showLogoutDialog.value = true
-   }
-
-   if (showLogoutDialog.value) {
-      LogoutBox(navController = navController, onDismiss = { showLogoutDialog.value = false })
-   }
-   val uiScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
-
-   fun runOnUiThread(block: suspend () -> Unit) = uiScope.launch { block() }
-
    return DrawerState(DrawerValue.Closed)
 }
 
@@ -121,7 +156,7 @@ fun NovedadesScreen(navController: NavHostController): DrawerState {
 @Preview
 @Composable
 fun NovedadesScreenPreview() {
-   //NovedadesScreen()
+   NovedadesScreen(navController = rememberNavController())
 }
 
 
