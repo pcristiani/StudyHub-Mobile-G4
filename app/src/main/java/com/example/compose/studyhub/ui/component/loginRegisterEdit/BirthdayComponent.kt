253ac4a8b43ba0fe232a709dc267a5cae67b3ffe
ElDeLaBarba <thatonewithabeard@gmail.com>
2024-06-27 11:01:54 -0300

    Changed date component to match other fields style

--- app/src/main/java/com/example/compose/studyhub/ui/component/loginRegisterEdit/BirthdayComponent.kt
+++ app/src/main/java/com/example/compose/studyhub/ui/component/loginRegisterEdit/BirthdayComponent.kt
@@ -3,6 +3,7 @@ package com.example.compose.studyhub.ui.component.loginRegisterEdit
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +14,7 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -20,9 +22,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.studyhub.R
@@ -36,23 +40,28 @@ import java.util.Locale
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun Birthday(birthdayState: DatePickerState, imeAction: ImeAction = ImeAction.Next, onImeAction: () -> Unit = {}) {
-  Text(text = stringResource(id = R.string.birthday), style = MaterialTheme.typography.bodyMedium)
   Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
     val showDatePicker = remember { mutableStateOf(false) }
-    val selectedDate = remember { mutableStateOf<Date?>(null) } // this will store whatever date the user selects // Button to show the date picker dialog
+    val selectedDate =
+      remember { mutableStateOf<Date?>(null) } // this will store whatever date the user selects // Button to show the date picker dialog
     val sdf = SimpleDateFormat("MMM dd,yyyy", Locale.getDefault())
     val currentDate = Date(System.currentTimeMillis())
-    Button(onClick = { showDatePicker.value = true }, modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(5.dp), colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.white), contentColor = colorResource(R.color.text_black)), border = BorderStroke(1.dp, colorResource(R.color.darker_gray))) {
-      if (UserRepository.user == User.NoUserLoggedIn) {
-        val displayDate = selectedDate.value ?: currentDate
-        Text(text = sdf.format(displayDate), style = MaterialTheme.typography.bodyMedium, modifier = Modifier.padding(top=10.dp, bottom=10.dp))
-      } else {
-        val fechaNacimiento = UserRepository.getFechaNacimiento()
-        if (fechaNacimiento != null) {
-          Text(text = reformatDate(fechaNacimiento), style = MaterialTheme.typography.bodyMedium)
-        }
-      }
+
+    var displayDate: String
+
+    if (UserRepository.user == User.NoUserLoggedIn) {
+      displayDate = selectedDate.value?.let { sdf.format(it) } ?:""
+    } else {
+      displayDate = UserRepository.getFechaNacimiento()?.let { reformatDate(it) }?:""
     }
+
+
+
+    OutlinedTextField(value = displayDate, onValueChange={}, readOnly= true, label = {
+      Text(text = stringResource(id = R.string.birthday), style = MaterialTheme.typography.bodyMedium)
+
+    }, modifier = Modifier.fillMaxWidth().onFocusChanged { focusState -> if(focusState.isFocused){showDatePicker.value = true } })
+
 
     if (showDatePicker.value) {
       DatePickerDialog(onDismissRequest = { showDatePicker.value = false }, confirmButton = {
@@ -60,11 +69,11 @@ fun Birthday(birthdayState: DatePickerState, imeAction: ImeAction = ImeAction.Ne
           showDatePicker.value = false
           selectedDate.value = Date(birthdayState.selectedDateMillis ?: 0)
         }, enabled = birthdayState.selectedDateMillis != null) {
-          Text(text = "Confirm")
+          Text(text = "Confirmar")
         }
       }, dismissButton = {
         TextButton(onClick = { showDatePicker.value = false }) {
-          Text(text = "Dismiss")
+          Text(text = "Cancelar")
         }
       }) {
         DatePicker(state = birthdayState)
