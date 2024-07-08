17505fb92ab8d87222ae8b833d345e712621a35d
ElDeLaBarba <thatonewithabeard@gmail.com>
2024-07-08 15:15:38 -0300

    Merge

---
+++ app/src/main/java/com/example/compose/studyhub/ui/viewModel/InicioViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.compose.studyhub.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.compose.studyhub.data.UserRepository
+
+class InicioViewModel(private val userRepository: UserRepository) : ViewModel() {
+    fun handleContinue(ci: String, onNavigateToLogin:(email: String) -> Unit) {
+        onNavigateToLogin(ci)
+    }
+}
+
+class InicioViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InicioViewModel::class.java)) {
+            return InicioViewModel(UserRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
