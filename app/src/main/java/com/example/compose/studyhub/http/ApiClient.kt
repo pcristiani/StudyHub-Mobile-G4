7c4be0046191a252232099a06a13817a9600a6c0
Sebastiangonlai <58099324+Sebastiangonlai@users.noreply.github.com>
2024-07-19 00:04:35 -0300

    url produccion

--- app/src/main/java/com/example/compose/studyhub/http/ApiClient.kt
+++ app/src/main/java/com/example/compose/studyhub/http/ApiClient.kt
@@ -3,8 +3,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitClient {
-   private const val BASE_URL = "http://10.0.2.2:8080/"
-   //private const val BASE_URL = "https://studyhub-backend-production.up.railway.app"
+  // private const val BASE_URL = "http://10.0.2.2:8080/"
+   private const val BASE_URL = "https://studyhub-backend-production.up.railway.app"
 
    val instance: Retrofit by lazy {
       Retrofit.Builder()
